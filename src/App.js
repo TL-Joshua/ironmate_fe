58cd058bdefcaa592610b153f6cce98401f64e1f
joshua.nguyen <joshua.nguyen@grapecheck.de>
2022-08-10 20:03:23 +0200

    Add some stuff for testing

--- src/App.js
+++ src/App.js
@@ -4,6 +4,16 @@ function App() {
   return (
     <div className="App">
         <h1>🔥 IronMate</h1>
+        <h2>Work in progress</h2>
+        <div className='infobox'>
+          <hr className='infobox'/>
+          <h3 >To-Dos:</h3>
+          <ul>
+            <li>Learn React</li>
+            <li>Plan features</li>
+            <li>Start working</li>
+          </ul>
+        </div>
     </div>
   );
 }
