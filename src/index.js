0650bf8b1df90a0c5f41e0fc17078b6c3666ec2f
joshua.nguyen <joshua.nguyen@grapecheck.de>
2022-08-17 17:25:12 +0200

    feature(router): Install basic react router

--- src/index.js
+++ src/index.js
@@ -4,10 +4,18 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App/>}/>
+        <Route path="/login" element={<h1>LOGIN Component kommt hier rein</h1>}/>
+        <Route path="*" element={<p>NOTHING HERE</p>}/> {/* sog. empty Route */}
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
