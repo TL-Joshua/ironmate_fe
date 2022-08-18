a6762fc877398cafecedce7200cc91a935a01ea6
Kamil Chahrour <kamil.chahrour@stud.uni-due.de>
2022-08-18 14:11:12 +0200

    userinfo

---
+++ src/MatchingMain.js
@@ -0,0 +1,23 @@
+import UserPic from "./UserPic";
+import UserInfo from "./UserInfo";
+import ButtonsYesNo from "./ButtonsYesNo";
+import { useEffect, useState } from "react";
+const MatchingMain = () => {
+    
+    return ( 
+        <div className="Matching">
+            <h1>Find The Perfect Mate</h1>
+            <h2>Ironmates in your Area:</h2>
+                <UserInfo/>
+                <UserPic />         
+                <ButtonsYesNo/>
+                
+        </div>
+
+        
+     );
+    
+}
+
+ 
+export default MatchingMain;
