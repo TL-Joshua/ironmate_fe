1b2a1e71d940887a923bff76e0184b004583aedb
joshua.nguyen <joshua.nguyen@grapecheck.de>
2022-08-19 17:36:00 +0200

    feature: Add simple Link to /signup

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
         <h1>🔥 IronMate Homepage</h1>
         <Link to={"/login"}>LOGIN HERE</Link>
+        <Link to={"/signup"}>Sign up HERE</Link>
     </div>
   );
 }
