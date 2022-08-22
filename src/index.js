8944257529c9d9dfaa27f6d6ac56ccc959e8afe7
joshua.nguyen <joshua.nguyen@grapecheck.de>
2022-08-22 12:43:42 +0200

    Fix index.js

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Matching from './Matching';
-import reportWebVitals from './reportWebVitals';
 import Login from "./Login/Login"
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
@@ -12,7 +10,6 @@ import SignUp from './Signup/Signup';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Matching />
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App/>}/>
