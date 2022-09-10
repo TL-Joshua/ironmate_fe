abaaf0642e91c75cbe96d675845d7f7159c0a9fa
Kamil Chahrour <kamil.chahrour@stud.uni-due.de>
2022-09-10 20:03:49 +0200

    landing page css

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -6,16 +6,23 @@ const Home = () => {
 
     return (
         <div className="Matching">
+        
         <Navbar/>
+        <div className="Content">
+        <div className="Info1">        
         <div className="firstPic"><img src="https://i.pinimg.com/736x/d0/22/01/d022016656b3d136b5e46b7c76baa1f0--gym-workouts-ball-workouts.jpg" alt=""></img></div>
-        <h1>About us</h1>
-            <h2>IronMate is the opportunity for you to meet people with same interests and actively work with them towards your goals.
+            <h3>        
+                <h1>About us</h1>
+                IronMate is the opportunity for you to meet people with same interests and actively work with them towards your goals.
                 Through our unique matching system, we put you in touch with more than 100 other people interested in fitness.
                 Our coaches and individual fitness programmes help you optimise your tracking plan by creating your tasks and tracking your working time.
-            </h2>
-        <div className="container"><div><img src="https://pbs.twimg.com/media/ERvUtsZXUAA5OVI.jpg" alt="" /></div></div>
-        <div className="info"><h1>Your benefits with Iron Mate</h1>
+            </h3>
+            </div>
+
+        <div className="Info2">
+        <div className="secondPic"><img src="https://pbs.twimg.com/media/ERvUtsZXUAA5OVI.jpg" alt="" /></div>
             <h3>
+            <h1>Your benefits with IronMate</h1>
                 <ul className="homeUL">
                 <li>discipline!</li>
                 <li>motivation!</li>
@@ -23,16 +30,22 @@ const Home = () => {
                 <li>new friendships!</li>
                 <li>more fun!</li></ul>
             </h3></div>
-        <div className="container"><img src="https://pbs.twimg.com/media/DcPuFHUWkAAB2ci.jpg:large" alt="" /></div>
+        
+
+        <div className="Info3">        
+        <div className="thirdPic"><img src="https://pbs.twimg.com/media/DcPuFHUWkAAB2ci.jpg:large" alt=""/></div>
+        <h3>        
         <h1>Become an Iron Club Member</h1>
-        <h3>
         <ul className="memberUL">
                 <li>Standard Membership</li>
                 <li>Gold Membership</li>
                 <li>Iron Membership</li>
                 <p>MORE INFOS COMING SOON</p>
         </ul>
-        </h3>
+        </h3>                
+        </div>
+
+        </div>
         </div>
 
     );
